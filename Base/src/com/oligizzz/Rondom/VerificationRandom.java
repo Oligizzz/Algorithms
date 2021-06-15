47c1bbb64917c9aee748c1d5b4d8aad75b77c64c
Oligizzz <80525269+Oligizzz@users.noreply.github.com>
2021-06-15 16:12:37 +0800

    Delete Base/src/com/oligizzz/Rondom directory

--- Base/src/com/oligizzz/Rondom/VerificationRandom.java
+++
@@ -1,7 +0,0 @@
-package com.oligizzz.Rondom;
-
-public interface VerificationRandom {
-    public void setSamples(int m);//���������ռ��С
-    public double [] verificationRandom(int m);
-   //��֤0��m(������m)���ֵĸ����Ƿ��������������ĸ���
-}
